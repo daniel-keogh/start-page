a7b927ddd2df792f72863ee94194abb9aaad3e1e
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-09-24 19:26:29 +0100

    Minor changes

--- src/scripts/searchEngines.js
+++ src/scripts/searchEngines.js
@@ -1,174 +1,38 @@
 const searchEngines = [
-    {
-        name: "AlternativeTo",
-        url: "https://alternativeto.net/browse/search/?q=",
-        key: "alt"
-    },
-    {
-        name: "Amazon UK",
-        url: "https://www.amazon.co.uk/s?k=",
-        key: "am"
-    },
-    {
-        name: "Bing",
-        url: "https://www.bing.com/search?q=",
-        key: "b"
-    },
-    {
-        name: "Deezer",
-        url: "https://www.deezer.com/search/",
-        key: "dz"
-    },
-    {
-        name: "DuckDuckGo",
-        url: "https://duckduckgo.com/&q=",
-        key: "d"
-    },
-    {
-        name: "Ecosia",
-        url: "https://www.ecosia.org/search?q=",
-        key: "ec"
-    },
-    {
-        name: "Firefox Add-ons",
-        url: "https://addons.mozilla.org/en-US/firefox/search/?q=",
-        key: "ad"
-    },
-    {
-        name: "Genius",
-        url: "https://genius.com/search?q=",
-        key: "gs"
-    },
-    {
-        name: "Google",
-        url: "https://www.google.com/search?&q=",
-        key: "g"
-    },
-    {
-        name: "Google Maps",
-        url: "https://www.google.com/maps/search/",
-        key: "gm"
-    },
-    {
-        name: "Google News",
-        url: "https://news.google.com/search?q=",
-        key: "gn"
-    },
-    {
-        name: "Google Translate",
-        url: "https://translate.google.com/#auto|auto|",
-        key: "gt"
-    },
-    {
-        name: "GitHub",
-        url: "https://github.com/search?q=",
-        key: "gh"
-    },
-    {
-        name: "Goodreads",
-        url: "https://www.goodreads.com/search?q=",
-        key: "gr"
-    },
-    {
-        name: "IMDb",
-        url: "https://www.imdb.com/find?s=all&q=",
-        key: "imdb"
-    },
-    {
-        name: "Last.fm",
-        url: "https://www.last.fm/search?q=",
-        key: "lfm"
-    },
-    {
-        name: "Medium",
-        url: "https://medium.com/search?q=",
-        key: "m"
-    },
-    {
-        name: "MDN",
-        url: "https://developer.mozilla.org/en-US/search?q=",
-        key: "mdn"
-    },
-    {
-        name: "Merriam Webster",
-        url: "https://www.merriam-webster.com/dictionary/",
-        key: "mw"
-    },
-    {
-        name: "NPM",
-        url: "https://www.npmjs.com/search?&q=",
-        key: "npm"
-    },
-    {
-        name: "OpenStreetMap",
-        url: "https://www.openstreetmap.org/search?query=",
-        key: "osm"
-    },
-    {
-        name: "PyPI",
-        url: "https://pypi.org/search/?q=",
-        key: "pip"
-    },
-    {
-        name: "Pixabay",
-        url: "https://pixabay.com/images/search/",
-        key: "px"
-    },
-    {
-        name: "Qwant",
-        url: "https://www.qwant.com/?q=",
-        key: "q"
-    },
-    {
-        name: "Reddit",
-        url: "https://old.reddit.com/search?q=",
-        key: "r"
-    },
-    {
-        name: "Spotify",
-        url: "https://open.spotify.com/search/",
-        key: "sp"
-    },
-    {
-        name: "StackExchange",
-        url: "https://stackoverflow.com/search?q=",
-        key: "so"
-    },
-    {
-        name: "StackOverflow",
-        url: "https://stackexchange.com/search?q=",
-        key: "se"
-    },
-    {
-        name: "StartPage",
-        url: "https://www.startpage.com/do/dsearch?query=",
-        key: "s"
-    },
-    {
-        name: "Twitter",
-        url: "https://twitter.com/search?q=",
-        key: "t"
-    },
-    {
-        name: "Unsplash",
-        url: "https://unsplash.com/s/photos/",
-        key: "un"
-    },
-    {
-        name: "Wikipedia",
-        url: "https://en.wikipedia.org/w/index.php?&search=",
-        key: "w"
-    },
-    {
-        name: "Yandex",
-        url: "https://yandex.com/search/?text=",
-        key: "yx"
-    },
-    {
-        name: "YouTube",
-        url: "https://www.youtube.com/results?search_query=",
-        key: "y"
-    },
+    { name: "AlternativeTo", url: "https://alternativeto.net/browse/search/?q=", key: "alt" },
+    { name: "Amazon UK", url: "https://www.amazon.co.uk/s?k=", key: "am" },
+    { name: "Bing", url: "https://www.bing.com/search?q=", key: "b" },
+    { name: "Deezer", url: "https://www.deezer.com/search/", key: "dz" },
+    { name: "DuckDuckGo", url: "https://duckduckgo.com/&q=", key: "d" },
+    { name: "Ecosia", url: "https://www.ecosia.org/search?q=", key: "ec" },
+    { name: "Firefox Add-ons", url: "https://addons.mozilla.org/en-US/firefox/search/?q=", key: "ad" },
+    { name: "Genius", url: "https://genius.com/search?q=", key: "gs" },
+    { name: "Google", url: "https://www.google.com/search?&q=", key: "g" },
+    { name: "Google Maps", url: "https://www.google.com/maps/search/", key: "gm" },
+    { name: "Google News", url: "https://news.google.com/search?q=", key: "gn" },
+    { name: "Google Translate", url: "https://translate.google.com/#auto|auto|", key: "gt" },
+    { name: "GitHub", url: "https://github.com/search?q=", key: "gh" },
+    { name: "Goodreads", url: "https://www.goodreads.com/search?q=", key: "gr" },
+    { name: "IMDb", url: "https://www.imdb.com/find?s=all&q=", key: "imdb" },
+    { name: "Last.fm", url: "https://www.last.fm/search?q=", key: "lfm" },
+    { name: "Medium", url: "https://medium.com/search?q=", key: "m" },
+    { name: "MDN", url: "https://developer.mozilla.org/en-US/search?q=", key: "mdn" },
+    { name: "Merriam Webster", url: "https://www.merriam-webster.com/dictionary/", key: "mw" },
+    { name: "NPM", url: "https://www.npmjs.com/search?&q=", key: "npm" },
+    { name: "OpenStreetMap", url: "https://www.openstreetmap.org/search?query=", key: "osm" },
+    { name: "PyPI", url: "https://pypi.org/search/?q=", key: "pip" },
+    { name: "Pixabay", url: "https://pixabay.com/images/search/", key: "px" },
+    { name: "Qwant", url: "https://www.qwant.com/?q=", key: "q" },
+    { name: "Reddit", url: "https://old.reddit.com/search?q=", key: "r" },
+    { name: "Spotify", url: "https://open.spotify.com/search/", key: "sp" },
+    { name: "StackExchange", url: "https://stackoverflow.com/search?q=", key: "so" },
+    { name: "StackOverflow", url: "https://stackexchange.com/search?q=", key: "se" },
+    { name: "StartPage", url: "https://www.startpage.com/do/dsearch?query=", key: "s" },
+    { name: "Twitter", url: "https://twitter.com/search?q=", key: "t" },
+    { name: "Unsplash", url: "https://unsplash.com/s/photos/", key: "un" },
+    { name: "Wikipedia", url: "https://en.wikipedia.org/w/index.php?&search=", key: "w" },
+    { name: "Yandex", url: "https://yandex.com/search/?text=", key: "yx" },
+    { name: "YouTube", url: "https://www.youtube.com/results?search_query=", key: "y" },
 ];
 
 export default searchEngines;
